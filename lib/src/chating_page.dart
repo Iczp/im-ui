592dc2f075e0666eb7070e3a87703149d43ec020
zhongpei <1000@intry.cn>
2022-09-26 11:46:41 +0800

    00

---
+++ lib/src/chating_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ChatingPage extends StatefulWidget {
+  const ChatingPage({super.key});
+
+  @override
+  State<ChatingPage> createState() => _ChatingPageState();
+}
+
+class _ChatingPageState extends State<ChatingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
